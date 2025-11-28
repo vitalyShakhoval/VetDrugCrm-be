08101b065e55eca78822611762f6ffabd6dfa653
PavelitelDemonov <demonovdemonic@gmail.com>
2025-11-29 03:08:31 +0500

    переделал всё, сделал пару тестов postman потыкать его, надо разрешения и jwt докручивать

--- users/models.py
+++ users/models.py
@@ -3,6 +3,16 @@ from django.db import models
 
 class EmployeProfile(AbstractUser):
     username = None
+    USER_ROLES = [
+        ('manager', 'Менеджер'),
+        ('warehouse_supervisor', 'Заведующий складом'),
+        ('veterinarian', 'Ветеринар'),
+    ]
+    role = models.CharField(
+        max_length=50,
+        choices=USER_ROLES,
+        default='veterinarian'
+    )
     email = models.EmailField(unique=True, blank=False)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     
