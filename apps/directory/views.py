39662804a71d257220607946666c11bfe62cc958
Marat-Kurmakaev <kurmakaev.marik@gmail.com>
2025-11-28 13:57:06 +0500

    Добавил базовую фильтрацию

--- apps/directory/views.py
+++ apps/directory/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Drug
 from .serializers import DrugSerializer
 
@@ -6,3 +6,8 @@ from .serializers import DrugSerializer
 class DrugViewSet(viewsets.ModelViewSet):
     queryset = Drug.objects.all()
     serializer_class = DrugSerializer
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ["name", "code"]
+    ordering_fields = ["name", "code"]
+    ordering = ["name"]
