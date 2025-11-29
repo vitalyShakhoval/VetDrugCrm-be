79a4d6b733ba105cb1b994e3c6fb29ed12bb59f4
PavelitelDemonov <demonovdemonic@gmail.com>
2025-11-29 20:04:08 +0500

    добавил библиотеку с jwt и подготовил, новая версия

--- config/settings/base.py
+++ config/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     
+    'rest_framework_simplejwt',
     "rest_framework",
 
     'rest_framework.authtoken', # удалить
