33a32fd857c965857dd375d980476d6b281f39dc
PavelitelDemonov <demonovdemonic@gmail.com>
2025-12-12 14:39:09 +0500

    Update serializers.py

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from rolepermissions.roles import assign_role
+
 from .models import EmployeeProfile
 from .roles import get_role_class, get_role_choices
+
+
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     role = serializers.ChoiceField(choices=get_role_choices(), required=False)
