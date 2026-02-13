1b710d840716b75162cd566ef0ca962b0384b276
PavelitelDemonov <demonovdemonic@gmail.com>
2026-02-13 14:39:01 +0500

    первые шаги на импорт из excel

--- apps/batches/views.py
+++ apps/batches/views.py
@@ -1,8 +1,15 @@
-from rest_framework import viewsets, filters
+import pandas as pd
+from rest_framework import viewsets, filters, status
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from django.http import HttpResponse
+from django.db import transaction
+from drugs.models import Drug
+
 from .models import Batch
 from .serializers import BatchSerializer
-import pandas as pd
-from django.http import HttpResponse
+
 
 class BatchViewSet(viewsets.ModelViewSet):
     queryset = Batch.objects.all()
@@ -13,11 +20,80 @@ class BatchViewSet(viewsets.ModelViewSet):
     ordering_fields = ["expiry_date", "batch_number"]
     ordering = ["expiry_date"]
 
-def export_batch_to_excel(request):
-    batch = Batch.objects.all().values()
+class BatchImportView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+    
+    def post(self, request):
 
-    df = pd.DataFrame(list(batch))
+        file = request.FILES.get('file')
+        dry_run = request.data.get('dry_run', 'true').lower() == 'true'
 
-    respone = HttpResponse(content = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    respone['Content-Disposition'] = 'attachment; filename=persons.xlsx'
-    df.to_excel(respone,index=False)
+        if not file:
+            return Response({'error':'file required'}, status= status.HTTP_400_BAD_REQUEST)
+        try:
+            if file.name.endswith('.csv'):
+                df = pd.read_csv(file)
+            elif file.name.endswith('.xlsx'):
+                df = pd.read_excel(file)
+            else:
+                return Response({'error': 'unsupported file format'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(
+                {'error':f'file parse error: {e}'},
+                status=status.HTTP_400_BAD_REQUEST  
+            )
+        
+        required_columns = {
+            'drug',
+            'batch_number',
+            'remaining_quantity',
+            'expiry_date',
+            'supplier',
+            'purchase_price',
+        }
+
+        if not required_columns.issubset(df.columns):
+            return Response(
+                {'error':f'Missing required columns: {required_columns}'},
+                status= status.HTTP_400_BAD_REQUEST
+                )
+        
+        parsed_item = []
+
+        
+        #выборка нужных столбцов и их проверка на валидность            
+        for index,row in df.iterrows():
+            
+            if not Drug.objects.filter(name=row['drug']).exists():
+                return Response(
+                {'error':f"Missing required drugs: {row['drug']}"},
+                status= status.HTTP_400_BAD_REQUEST
+            )
+
+            item_data = {
+                'name': row['drug'],
+                'series':row['batch_number'],
+                'quantity':int(row['remaining_quantity']),
+                'expiry_date':pd.to_datetime(row['expiry_date']).date(),
+                'supplier':row['supplier'],
+                'purchase_price':row['purchase_price'],
+            }
+            #проверка есть ли название таблеток в списке
+            parsed_item.append(item_data)
+
+#сохранение
+            
+        if not dry_run:
+            with transaction.atomic():
+                for item in parsed_item:
+                    batch = Batch.objects.create(
+                        drug=Drug.objects.filter(name=item['name']).exists(),
+                        series=item['remaning_quantity'],
+                        quantity=item['quantity'],
+                        expiration_date=item['expiry_date'],
+                        supplier=item['supplier'],
+                        price=row.item('purchase_price'),
+                    )
+            return Response(parsed_item)
+        
+                
